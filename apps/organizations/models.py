3f1ae1e0a4b310f201732f792ffec1be6b7c9a42
caiovictormc <caiovictormc@gmail.com>
2019-02-11 16:36:18 -0300

    organizations: Create Organization model

--- apps/organizations/models.py
+++ apps/organizations/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+from apps.common.models import DefaultModel
+
+
+class Organization(DefaultModel):
+    name = models.CharField(max_length=75)
+    members = models.ManyToManyField(User, related_name='organizations')
+
+    def __str__(self):
+        return f'{self.name}'
