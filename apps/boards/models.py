70c9505e6457d1182e5d15edcf010c67adf5e306
caiovictormc <caiovictormc@gmail.com>
2019-02-12 15:05:23 -0300

    apps.boards: Create Board Model

--- apps/boards/models.py
+++ apps/boards/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+from apps.organizations.models import Organization
+
+import jwt
+
+
+class Board(models.Model):
+    organization = models.ForeignKey(
+        Organization, related_name="boards",
+        on_delete=models.SET_NULL, null=True, blank=True)
+    key = models.CharField(max_length=275)
+    board_number = models.IntegerField()
+    url = models.URLField()
+
+    def __str__(self):
+        return f'[{self.id}] {self.key}'
+
+    @property
+    def address(self):
+        payload = {
+            "resource": {"dashboard": self.board_number},
+            "params": {}
+        }
+        token = jwt.encode(payload, self.key, algorithm="HS256")
+        return self.url + "/embed/dashboard/" + token.decode("utf8") + "#bordered=true&titled=true"
